c6108da6edc6987512f7a7102112244a555eb87c
bradleymont <bradleymont@gmail.com>
2019-05-14 01:38:15 -0700

    tried to do less checks for length but seemed to slow down program, sticking with original implementation, passes test cases

--- Homework 4/tower.pl
+++ Homework 4/tower.pl
@@ -1,21 +1,16 @@
 basic_row_and_col_restrictions(GridSize,RowOrCol) :-
-    length(RowOrCol,GridSize), %each list in T is of length N
-    fd_domain(RowOrCol,1,GridSize), %each list in T contains integers from 1 to N
-    fd_all_different(RowOrCol). %each list in T contains distinct integers
+    length(RowOrCol,GridSize), % each list in T is of length N
+    fd_domain(RowOrCol,1,GridSize), % each list in T contains integers from 1 to N
+    fd_all_different(RowOrCol). % each list in T contains distinct integers
 
 
-%length_with_reversed_arguments(N,L) :- length(L,N).
+% I tried only checking for lists of length N once, but it seemed to slow down the program
 
 tower(N,T,C) :-
-    N >= 0, %N is a nonnegative integer
+    N >= 0, % N is a nonnegative integer
     length(T,N), % T must contain N lists
 
-
-    %maplist(length_with_reversed_arguments(N),T), %each list in T is of length N
-
-
-
-    maplist(basic_row_and_col_restrictions(N),T), %cimpose basic restrictions upon the rows
+    maplist(basic_row_and_col_restrictions(N),T), % impose basic restrictions upon the rows
     transpose(T,T_transpose),
     maplist(basic_row_and_col_restrictions(N),T_transpose), % impose basic restrictions upon the columns
 
@@ -39,32 +34,12 @@ tower(N,T,C) :-
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 % N is a nonnegative integer specifying the size of the square grid
 % T, a list of N lists, represents each row of the square grid
 % each row is a list of N distinct integers from 1 to N (same with columns)
 % C is a structure with function symbol counts and arity 4
 % C's arguments are lists of N integers, representing the
 % tower counts for top, bottom, left, right respectively
-
-
-
-
-
-
-
-
-
 
 
 /* RULES TO CHECK TOWER COUNT */
