ea49b71dde64d594337da327950e6cf2f3a92f02
bradleymont <bradleymont@gmail.com>
2019-05-14 22:30:22 -0700

    passes cases, last commit before cleaning up code

--- Homework 4/tower.pl
+++ Homework 4/tower.pl
@@ -3,14 +3,6 @@ basic_row_and_col_restrictions(GridSize,RowOrCol) :-
     fd_domain(RowOrCol,1,GridSize), % each list in T contains integers from 1 to N
     fd_all_different(RowOrCol). % each list in T contains distinct integers
 
-
-% N is a nonnegative integer specifying the size of the square grid
-% T, a list of N lists, represents each row of the square grid
-% each row is a list of N distinct integers from 1 to N (same with columns)
-% C is a structure with function symbol counts and arity 4
-% C's arguments are lists of N integers, representing the
-% tower counts for top, bottom, left, right respectively
-
 tower(N,T,C) :-
     N >= 0, % N is a nonnegative integer
     length(T,N), % T must contain N lists
@@ -123,6 +115,12 @@ transpose(X,[Y_head|Y_tail]) :-
 
 
 
+
+
+
+
+
+
 % starting plain_tower --------------------------------
 
 unique_decreasing_list([],0).
@@ -132,56 +130,13 @@ unique_decreasing_list([H|T],N) :-
     N_decremented is N - 1,
     unique_decreasing_list(T,N_decremented).
 
-% from TA Kimmo's slides
-
-elements_between([],_,_).
-elements_between([H|T],Min,Max) :-
-    between(Min,Max,H),
-    elements_between(T,Min,Max).
-
-all_unique([]).
-all_unique([H|T]) :- member(H,T),!,fail.
-all_unique([_|T]) :- all_unique(T).
-
-
-/*
-
-% list L has unique elements between 1 and N
-unique_list(N,L) :-
-    length(L,N), % each list in T is of length N
-    elements_between(L,1,N), % each list in T contains integers from 1 to N
-    all_unique(L),!. % each list in T contains distinct integers
-
-*/
-
-
-% list L has unique elements between 1 and N
-unique_list(N,L) :-
-    unique_decreasing_list(DecreasingList,N),!,
-    permutation(DecreasingList,L).
-
-
-length_with_parameters_reversed(N,L) :- length(L,N).
-
-
-/* one list between Min and Max: L
-maplist(between(Min,Max),
-
-
-*/
-
-lbetween(L,Min,Max) :-
-    maplist(between(Min,Max),L).
-
-lolbetween(T,Min,Max) :-
-    maplist(maplist(between(Min,Max)),T).
 
 
 
 
-% NEW PLAIN TOWER
-%                     LE       RowOrCol
-% plain_check_forward([3,1,2],[[1,2,3],[3,1,2],[2,3,1]]).
+
+%  LE       RowOrCol
+% ([3,1,2],[[1,2,3],[3,1,2],[2,3,1]]).
 
 idk(UniqueList,N,RowOrCol,LeftElement) :-
     permutation(UniqueList,RowOrCol),
@@ -192,13 +147,13 @@ idk(UniqueList,N,RowOrCol,LeftElement) :-
 
 
 plain_tower(N,T,C) :-
-    N >= 0, % N is a nonnegative integer
-    length(T,N), % T must contain N lists
+    N >= 0,
+    length(T,N),
 
     length(UniqueList,N),
     unique_decreasing_list(UniqueList,N), % basic unique list, ex: [4,3,2,1]
 
-    C = counts(Top,Bottom,Left,Right), % check the counts on the edges
+    C = counts(Top,Bottom,Left,Right),
     length(Left,N),
     length(Right,N),
     length(Top,N),
@@ -220,46 +175,6 @@ plain_tower(N,T,C) :-
 
 
 
-
-/* OLD IMPLEMENTATION
-
-plain_tower(N,T,C) :-
-    N >= 0, % N is a nonnegative integer
-    length(T,N), % T must contain N lists
-
-
-    unique_decreasing_list(UniqueList,N),!, % basic unique list, ex: [4,3,2,1]
-
-    maplist(permutation(UniqueList),T), % try every possible permutation of the unique list
-
-    transpose(T,T_transpose),
-    maplist(unique_list(N),T_transpose), % make sure the columns are valid as well
-
-
-
-    C = counts(Top,Bottom,Left,Right), % check the counts on the edges
-    length(Left,N),
-    length(Right,N),
-    length(Top,N),
-    length(Bottom,N),
-
-
-    %elements_between(Left,1,N),
-    %elements_between(Right,1,N),
-    %elements_between(Top,1,N),
-    %elements_between(Bottom,1,N),
-
-
-
-
-    plain_check_forward(Left,T,N),
-    plain_check_backward(Right,T,N),
-    plain_check_forward(Top,T_transpose,N),
-    plain_check_backward(Bottom,T_transpose,N).
-
-*/
-
-
 % plain tower count
 
 plain_tower_count([],_,0).
@@ -268,23 +183,9 @@ plain_tower_count([],_,0).
 plain_tower_count([Front|Back],MaxHeight,NumVisible) :-
     Front > MaxHeight,
     NumVisMinusOne is NumVisible - 1,
-    plain_tower_count(Back,Front,NumVisMinusOne),!.
+    plain_tower_count(Back,Front,NumVisMinusOne).
 
 % rule for when the current tower is not greater than the current max height
 plain_tower_count([Front|Back],MaxHeight,NumVisible) :-
     Front < MaxHeight,
-    plain_tower_count(Back,MaxHeight,NumVisible),!.
-
-
-% plain_check_forward([3,1,2],[[1,2,3],[3,1,2],[2,3,1]]).
-
-plain_check_forward([],[],_).
-
-plain_check_forward([LeftHead|LeftTail],[CurrRow|OtherRows],N) :-
-    between(1,N,LeftHead),
-    plain_tower_count(CurrRow,0,LeftHead),
-    plain_check_forward(LeftTail,OtherRows,N).
-
-plain_check_backward(Right,T,N) :-
-    maplist(reverse,T,RevT),
-    plain_check_forward(Right,RevT,N).
+    plain_tower_count(Back,MaxHeight,NumVisible).
